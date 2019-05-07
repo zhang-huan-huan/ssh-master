56f0364f5f9f3aba9c69d03f90596012ddd8a26a
zhanghuanhuan <zhanghuanhaun@qq.com>
2019-05-07 11:17:11 +0800

    开发了一个testWeb类

---
+++ src/main/java/com/oracle/web/test/TestWeb.java
@@ -0,0 +1,12 @@
+package com.oracle.web.test;
+
+public class TestWeb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+
+	}
+
+}
