49169adf5b868b3e63e7b8104abb8578b0fdcbf2
zhanghuanhuan <zhanghuanhaun@qq.com>
2019-05-07 14:54:33 +0800

    Jackson新开发的功能

---
+++ src/main/java/com/oracle/web/test/TestAI.java
@@ -0,0 +1,12 @@
+package com.oracle.web.test;
+
+public class TestAI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("AI人工智能……");
+
+	}
+
+}
